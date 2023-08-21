f7df989a040739138860ad068dfcd2efb35e1d40
Dominik Gałenza <dominik.galenza@gmail.com>
2023-08-21 20:39:24 +0200

    Imported additional rooms, created scriptable objects for them.

--- Assets/Scripts/Dungeon/Doorway.cs
+++ Assets/Scripts/Dungeon/Doorway.cs
@@ -1,13 +1,12 @@
-using UnityEngine;
-
+﻿using UnityEngine;
 [System.Serializable]
-public class Doorway
+public class Doorway 
 {
     public Vector2Int position;
     public Orientation orientation;
     public GameObject doorPrefab;
     #region Header
-    [Header("The upper left position to start copying from")]
+    [Header("The Upper Left Position To Start Copying From")]
     #endregion
     public Vector2Int doorwayStartCopyPosition;
     #region Header
@@ -18,6 +17,8 @@ public class Doorway
     [Header("The height of tiles in the doorway to copy over")]
     #endregion
     public int doorwayCopyTileHeight;
-    [HideInInspector] public bool isConnected;
-    [HideInInspector] public bool isUnavailable;
+    [HideInInspector]
+    public bool isConnected = false;
+    [HideInInspector]
+    public bool isUnavailable = false;
 }
